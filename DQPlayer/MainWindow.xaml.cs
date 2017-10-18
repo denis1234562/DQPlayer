0a76d35245d9b59ca65d16fa17c44b9815f7c505
Denis <denis12251@abv.bg>
2017-10-18 18:48:40 +0300

    A lot of stuff

--- DQPlayer/MainWindow.xaml.cs
+++ DQPlayer/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
-using System.Threading;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DQPlayer.Annotations;
 using Microsoft.Win32;
 
 namespace DQPlayer
@@ -23,11 +11,18 @@ namespace DQPlayer
     public partial class MainWindow
     {
         private bool _isPlaying;
+        private MoviePlayerSourceTracker _moviePlayerTracker;
 
         public MainWindow()
         {
             InitializeComponent();
             SetupControls();
+            _moviePlayerTracker.PropertyChanged += Asd_PropertyChanged;
+        }
+
+        private void Asd_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            SourceChanged(((MediaElement)sender).Source != null);
         }
 
         #region Startup Methods
@@ -35,6 +30,7 @@ namespace DQPlayer
         {
             SetupControlTexts();
             SetupPlayer();
+            SourceChanged(false);
         }
 
         private void SetupControlTexts()
@@ -46,6 +42,7 @@ namespace DQPlayer
         {
             Player.LoadedBehavior = MediaState.Manual;
             Player.UnloadedBehavior = MediaState.Manual;
+            _moviePlayerTracker = new MoviePlayerSourceTracker(Player);
         }
         #endregion
 
@@ -69,6 +66,10 @@ namespace DQPlayer
             bChangeStatePlayer.Content = _isPlaying ? ResourceFiles.Strings.Pause : ResourceFiles.Strings.Resume;
         }
 
+        private void SourceChanged(bool state)
+        {
+            bChangeStatePlayer.IsEnabled = state;
+        }
         #endregion
 
         #region Event Handlers
@@ -79,7 +80,8 @@ namespace DQPlayer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
+            OpenFileDialog fileDialog =
+                new OpenFileDialog {Filter = "Matrioshka files (*.mkv)|*.mkv|Music files (*.mp3)|*.mp3"};
             bool? shown = fileDialog.ShowDialog();
             if (shown.HasValue && shown.Value)
             {
@@ -87,17 +89,59 @@ namespace DQPlayer
                 Player.Play();
                 _isPlaying = true;
                 UpdateChangeStateContent();
+                _moviePlayerTracker.MoviePlayerSource = Player.Source;
             }
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
-            string file = ((DataObject) e.Data).GetFileDropList()[0];
-            Player.Source = new Uri(file);
+            string filePath = ((DataObject) e.Data).GetFileDropList()[0];
+            string fileExtension = filePath.Substring(filePath.LastIndexOf(".", StringComparison.Ordinal));
+            if (!Settings.AllowedExtensions.Contains(fileExtension))
+            {
+                MessageBox.Show(this,
+                    $"Invalid file extension! Allowed file types are: {string.Join(",", Settings.AllowedExtensions)}",
+                    "Error");
+                return;
+            }
+            Player.Source = new Uri(filePath);
             Player.Play();
             _isPlaying = true;
             UpdateChangeStateContent();
+            _moviePlayerTracker.MoviePlayerSource = Player.Source;
         }
         #endregion
+    }
+
+    public class MoviePlayerSourceTracker : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly MediaElement _moviePlayer;
+
+        private Uri _moviePlayerSource;
+        public Uri MoviePlayerSource
+        {
+            get => _moviePlayerSource;
+            set
+            {
+                if (value != _moviePlayerSource)
+                {
+                    _moviePlayerSource = value;
+                    OnPropertyChanged(nameof(MoviePlayerSource));
+                }
+            }
+        }
+
+        public MoviePlayerSourceTracker(MediaElement moviePlayer)
+        {
+            _moviePlayer = moviePlayer;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(_moviePlayer, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
