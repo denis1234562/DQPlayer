c201dc48d3967b1912d74b735bef27d1526796e4
Denis <denis12251@abv.bg>
2017-10-20 02:49:35 +0300

    Minor improvements

--- DQPlayer/MainWindow.xaml.cs
+++ DQPlayer/MainWindow.xaml.cs
@@ -38,9 +38,6 @@ namespace DQPlayer
             Player.UnloadedBehavior = MediaState.Manual;
             _moviePlayerTracker = new MoviePlayerSourceTracker(Player);
             _moviePlayerTracker.PropertyChanged += MoviePlayer_PropertyChanged;
-            sVolumeSlider.ValueChanged += SVolumeSlider_ValueChanged;
-            sVolumeSlider.Value = 1;
-            Player.Volume = 1;
         }
         #endregion
 
@@ -61,45 +58,53 @@ namespace DQPlayer
 
         private void UpdateChangeStateContent()
         {
-            bChangeStatePlayer.Content = _isPlaying ? ResourceFiles.Strings.Pause : ResourceFiles.Strings.Resume;
+            bChangeStatePlayer.Content = _isPlaying
+                ? ResourceFiles.Strings.Pause
+                : ResourceFiles.Strings.Resume;
         }
 
         private void SourceChanged(bool state)
         {
             bChangeStatePlayer.IsEnabled = state;
         }
+
+        private void SetNewPlayerSource(Uri source)
+        {
+            Player.Source = source;
+            _moviePlayerTracker.MoviePlayerSource = source;
+        }
+
+        private void PlayNewPlayerSource(Uri source)
+        {
+            SetNewPlayerSource(source);
+            Player.Play();
+            _isPlaying = true;
+            UpdateChangeStateContent();
+        }
         #endregion
 
         #region Event Handlers
-        private void MoviePlayer_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            SourceChanged(((MediaElement)sender).Source != null);
-        }
 
-        private void bChangeStatePlayer_Click(object sender, RoutedEventArgs e)
-        {
-            SwitchPlayerState();
-        }
+        private void MoviePlayer_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            => SourceChanged(Player.Source != null);
+
+        private void bChangeStatePlayer_Click(object sender, RoutedEventArgs e) 
+            => SwitchPlayerState();
 
         private void bBrowse_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog =
-                new OpenFileDialog {Filter = "Matrioshka files (*.mkv)|*.mkv|Music files (*.mp3)|*.mp3"};
-            bool? shown = fileDialog.ShowDialog();
+            var fileDialog = new OpenFileDialog {Filter = "Matrioshka files (*.mkv)|*.mkv|Music files (*.mp3)|*.mp3"};
+            var shown = fileDialog.ShowDialog();
             if (shown.HasValue && shown.Value)
             {
-                Player.Source = new Uri(fileDialog.FileName);
-                Player.Play();
-                _isPlaying = true;
-                UpdateChangeStateContent();
-                _moviePlayerTracker.MoviePlayerSource = Player.Source;
+                PlayNewPlayerSource(new Uri(fileDialog.FileName));
             }
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
-            string filePath = ((DataObject) e.Data).GetFileDropList()[0];
-            string fileExtension = filePath.Substring(filePath.LastIndexOf(".", StringComparison.Ordinal));
+            var filePath = ((DataObject) e.Data).GetFileDropList()[0];
+            var fileExtension = filePath.Substring(filePath.LastIndexOf(".", StringComparison.Ordinal));
             if (!Settings.AllowedExtensions.Contains(fileExtension))
             {
                 MessageBox.Show(this,
@@ -107,17 +112,13 @@ namespace DQPlayer
                     "Error");
                 return;
             }
-            Player.Source = new Uri(filePath);
-            Player.Play();
-            _isPlaying = true;
-            UpdateChangeStateContent();
-            _moviePlayerTracker.MoviePlayerSource = Player.Source;
-        }
-
-        private void SVolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            Player.Volume = ((Slider)sender).Value;
+            PlayNewPlayerSource(new Uri(filePath));
         }
         #endregion
+
+        private void SMovieSkipSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //TODO
+        }
     }
 }
